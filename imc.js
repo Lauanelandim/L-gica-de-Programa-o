a24a4f3d1ed9e14f29f64c0264c5dac08c10b28a
Lauanelandim <landim0lauane@hotmail.com>
2024-09-04 21:34:10 -0300

    Add files via upload

---
+++ imc.js
@@ -0,0 +1,7 @@
+//Calcule o imc de uma pessoa , IMC=pes/(altura)²
+
+peso = 49;
+altura = 1.58;
+
+imc = peso / altura ** 2
+console.log("Seu imc é de: \n",imc.toFixed(2)) //reduzir número depois da vírgula
---
+++ nomecompleto.js
@@ -0,0 +1,13 @@
+//Declare duas variáveis chamadas "nome" e "sobrenome"
+//atribua o seu nome e sobrnome a elas. Concatene-as em uma terceira
+//variável chamada "nomeCompleto" e imprima ao usuário*/
+
+
+var nome;
+var sobrenome;
+
+nome = "Lauane"
+Sobrenome = "Landim"
+
+nomeCompleto = "Lauane Landim"
+console.log("Seu nome completo é :"+nomeCompleto);
